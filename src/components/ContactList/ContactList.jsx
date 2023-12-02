572d58f9a561ef24152909a9ccf4e5fe05d2a467
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-12-02 10:43:37 +0200

    css

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './ContactList.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
-    <ul>
+    <ul className="list-name">
       {contacts.map((contact) => (
         <li key={contact.id}>
           {contact.name}: {contact.number}
-          <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+          <button style={{ marginLeft: '10px' }} onClick={() => onDeleteContact(contact.id)}>Delete</button>
         </li>
       ))}
     </ul>
